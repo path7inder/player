4a2fecda5f0bae26fd43cf666e3ebd086ae8a915
path7inder <path7inder@gmail.com>
2022-08-25 00:08:00 +0900

    Add READ_EXTERNAL_STORAGE permission request

--- app/src/main/java/com/path7inder/player/MainActivity.java
+++ app/src/main/java/com/path7inder/player/MainActivity.java
@@ -1,14 +1,59 @@
 package com.path7inder.player;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String TAG = "MainActivity";
+    private final int REQUEST_CODE_PERMISSION = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (ContextCompat.checkSelfPermission(
+                getBaseContext(), Manifest.permission.READ_EXTERNAL_STORAGE) ==
+                PackageManager.PERMISSION_GRANTED) {
+            Log.d(TAG, "READ_EXTERNAL_STORAGE PERMISSION_GRANTED!");
+        } else if (shouldShowRequestPermissionRationale(
+                Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            Log.d(TAG, "User has denied READ_EXTERNAL_STORAGE explicitly!");
+        } else {
+            requestPermissions(
+                    new String[] { Manifest.permission.READ_EXTERNAL_STORAGE },
+                    REQUEST_CODE_PERMISSION
+            );
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CODE_PERMISSION) {
+            if (grantResults.length > 0 &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Log.d(TAG, "PERMISSION_GRANTED!");
+                ContentResolver contentResolver = getBaseContext().getContentResolver();
+                Cursor cursor = contentResolver.query(
+                        MediaStore.Video.Media.EXTERNAL_CONTENT_URI,
+                        null,
+                        null,
+                        null,
+                        null);
+                Log.d(TAG, "rows : " + cursor.getCount());
+                Log.d(TAG, "columns : " + cursor.getColumnCount());
+            }
+        }
     }
 }
